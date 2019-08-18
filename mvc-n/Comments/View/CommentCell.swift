ea0c75948833cd3863218d18d297166e48b171ec
kupriknikita <kupriknikita@gmail.com>
2019-08-18 19:37:25 +0300

    Create comment model

--- mvc-n/Comments/View/CommentCell.swift
+++ mvc-n/Comments/View/CommentCell.swift
@@ -6,4 +6,12 @@
 //  Copyright © 2019 Nikita Kuprik. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class CommentCell: UITableViewCell {
+    
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var textView: UITextView!
+    
+}
+
